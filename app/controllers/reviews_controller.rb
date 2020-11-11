d1729fa4b3caca48afa44672c2ce6eb4a4375306
Magda Barthofer <magda.barthofer@gmail.com>
2020-11-11 15:13:11 +0100

    added add restaurant and add review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,17 @@
 class ReviewsController < ApplicationController
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id]
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id]
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      elserender "new"
+      render "new"
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,4 @@ Rails.application.routes.draw do
     resources :reviews, only: [ :new, :create ]
   end
 end
+
