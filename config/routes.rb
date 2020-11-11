6bdaf2b845b47b3de5f00fcc5a40df700fb6e524
Magda Barthofer <magda.barthofer@gmail.com>
2020-11-11 15:58:19 +0100

    added dropdown to simple form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :restaurants, only: [ :index, :show, :new, :create ] do
-    resources :reviews, only: [ :new, :create ]
+  resources :restaurants do
+    resources :reviews
   end
 end
 
